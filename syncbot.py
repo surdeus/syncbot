bc6e0bd1d95b115a8167d7cd607205bfe554ac31
k1574 <k1574@yandex.ru>
2020-12-12 12:56:16 +0500

    Real init.

--- syncbot.py
+++ syncbot.py
@@ -3,28 +3,29 @@
 # Every bot polls the events of messages and sends 'em to the rest of bots.
 
 # VK API
-import vk_api, vk, json
-from vk_api.vk_keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.utils import get_random_id
+import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 # Telegram API
 import telebot
 
+import threading
+import json
+
 class ConvSyncBot:
 	# Basic class for chat bots in conversations.
 	def __init__(self, config, other_bots):
 		print("Initializing API.")
-	def send_text(self, text):
+	def send_text(self, username, text):
 		# The function to send just text messages in general chats
 		# of other bots.
-		print("Sending '%s'." % (text))
+		print("Sending '%s' from '%s'." % (text, username))
 	def run(self):
 		# The function that is supposed to be threaded.
-		print("Conversation bot is running.")
+		print("Conversation syncing bot is running.")
 
-class VKConvSyncBot(ConvSyncBot)
+class VKConvSyncBot(ConvSyncBot):
 	def __init__(self, config):
 		self.vk_token = config["vk_token"]
 		self.vk_group_id = int(config["vk_group_id"])
@@ -34,61 +35,71 @@ class VKConvSyncBot(ConvSyncBot)
 		self.chat_ids = config["vk_chat_ids"]
 		
 		# Ininialization.
-		self.vk_session = vk_api.VkApi(vk_token=self.vk_token)
+		self.vk_session = vk_api.VkApi(token=self.vk_token)
+		self.vk_longpoll = VkBotLongPoll(self.vk_session, self.vk_group_id)
 		self.vk = self.vk_session.get_api()
-		self.vk_longpoll = VkBotLongPoll(vk_session, vk_group_id)
-		
-		self.other_bots = []
 
-
-	def send_text(self, text):
-		for chat_id in chat_ids :
+	def send_text(self, username, text):
+		for chat_id in self.chat_ids :
 			self.vk.messages.send(
-				vk_key=self.vk_key, vk_server=vk_server, vk_ts=self.vk_ts,
-				random_id=get_random_id(), message=text, chat_id=chat_id
+				vk_key=self.vk_key, vk_server=self.vk_server, vk_ts=self.vk_ts,
+				random_id=vk_api.utils.get_random_id(),
+				message=("%s : %s")%(username, text),
+				chat_id=chat_id
 			)
+	def run(self):
+		for ev in self.vk_longpoll.listen() :
+			if not ev.chat_id in self.chat_ids :
+				return
+			if ev.type==VkBotEventType.MESSAGE_NEW and ev.from_chat :
+				print(ev)
+				from_id = ev.object.message['from_id']
+				user = self.vk.users.get(user_ids=from_id)
+				print(user)
+				username = ("%s %s" 
+				% (user[0]['first_name'], 
+					user[0]['last_name'])
+				)
+				print(username)
+				for bot in self.other_bots :
+					bot.send_text(username, ev.object["message"]["text"])
 		
-class TelegramConvSyncBot(ConvAPI)
+class TelegramConvSyncBot(ConvSyncBot):
 	def __init__(self, config):
 		self.t_token = config["t_token"]
 		self.chat_ids = config["t_chat_ids"]
-		self.t = telebot.TeleBot(t_token)
-		self.other_bots = []
-	
+		self.t = telebot.TeleBot(self.t_token)
 
-	def send_text(self, text):
-		for chat_id in chat_ids :
-			self.t.send_message(chat_id, text=text)
+	def send_text(self, username, text):
+		for chat_id in self.chat_ids :
+			self.t.send_message(chat_id, text="%s : %s" % (username, text))
 
 	def run(self):
+		@self.t.message_handler(content_types=['text'])
+		def on_text_message(message):
+			if not message.chat.id in self.chat_ids :
+				return
+			username="%s %s" % (message.from_user.first_name, message.from_user.last_name)
+			for bot in self.other_bots :
+				bot.send_text(username, message.text)
+		self.t.polling()
 		
-
-msg.chat.id
-
-@t_bot.message_handler(content_types['text'])
-def get_text_messages(msg):
-    t_bot.send_message(msg.chat.id, text=resptext)
-
-def handle_VK_events():
-    for event in longpoll.listen() :
-        if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat :
-        	for bot in other_bots :
-	        	bot.send_text(text)
-            vk.messages.send(vk_key=vk_key, vk_server=vk_server, vk_ts=vk_ts, random_id=get_random_id(),
-                message='Yes, it works. Stop typing.', chat_id = event.chat_id)
 def main():
 	config_path = "config.json"
 	with open(config_path, "r") as read_file :
 	    config = json.load(read_file)
+
 	bots = []
 	vk_bot = VKConvSyncBot(config)
 	telegram_bot = TelegramConvSyncBot(config)
+
 	vk_bot.other_bots = [telegram_bot]
 	telegram_bot.other_bots = [vk_bot]
+
 	bots = [vk_bot, telegram_bot]
-	# Threading.
 	for bot in bots :
-		
+		thread = threading.Thread(target=bot.run, args=())
+		thread.start()
 
 if __name__ == "__main__" :
 	main()
